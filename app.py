4bb5aaf32707f6f1399efd1aae8c7419cc6e9e91
maliahavlicek <malia.havlicek@gmail.com>
2020-01-06 08:35:36 -0700

    Initial commit - shell for flask, python, mongodb project

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+app = Flask(__name__)
+
+# before PROD put username password into env variables
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://root:2019Winter@mhavlicfirstcluster-pielp.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_events')
+def get_tasks():
+    return render_template('events.html')
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv("IP", "0.0.0.0"),
+            port=int(os.getenv("PORT", "5000")),
+            debug=True)
