20d55e4e13badbd2ffa3b246a16e1ed6d9e07e27
maliahavlicek <malia.havlicek@gmail.com>
2020-01-18 15:33:48 -0700

    add validation to phone, website and image url inputs.

--- forms.py
+++ forms.py
@@ -1,3 +1,5 @@
+import re
+
 from flask_wtf import FlaskForm
 from wtforms import (StringField,
                      TextAreaField,
@@ -5,12 +7,40 @@ from wtforms import (StringField,
                      PasswordField,
                      DateField,
                      SelectField,
-                     ValidationError)
+                     BooleanField,
+                     ValidationError, validators)
 from wtforms.validators import (DataRequired,
                                 Email,
                                 EqualTo,
                                 Length,
-                                URL)
+                                URL,
+                                Optional)
+from wtforms.fields.html5 import URLField
+
+class RequiredIf(DataRequired):
+    """Validator which makes a field required if another field is set and has a truthy value.
+    from thread in https://gist.github.com/devxoul/7638142
+    Sources:
+        - http://wtforms.simplecodes.com/docs/1.0.1/validators.html
+        - http://stackoverflow.com/questions/8463209/how-to-make-a-field-conditionally-optional-in-wtforms
+        - https://gist.github.com/devxoul/7638142#file-wtf_required_if-py
+    """
+    field_flags = ('requiredif',)
+
+    def __init__(self, message=None, *args, **kwargs):
+        super(RequiredIf).__init__()
+        self.message = message
+        self.conditions = kwargs
+
+    # field is requiring that name field in the form is data value in the form
+    def __call__(self, form, field):
+        for name, data in self.conditions.items():
+            other_field = form[name]
+            if other_field is None:
+                raise Exception('no field named "%s" in form' % name)
+            if other_field.data == data and not field.data:
+                DataRequired.__call__(self, form, field)
+            Optional()(form, field)
 
 
 class PlaceForm(FlaskForm):
@@ -20,14 +50,18 @@ class PlaceForm(FlaskForm):
         Length(min=1, message='Email is required.'),
         Email(message='Not a valid email address.')
     ])
-    name = StringField('Name of Place', [
+    name = StringField('Name of Place *', [
         Length(min=1, message='Name of Places is required.')
     ])
-    description = TextAreaField('Description', [
+    description = TextAreaField('Description *', [
         Length(min=1, message="Description is required"),
         Length(min=2, message='Your description is too short')
     ])
-    activity_place = SelectField(u'Activity', choices=[('none','Choose Activity')], validators=[])
+    activity_place = SelectField(u'Activity *', choices=[('none', 'Choose Activity')], validators=[])
+    phone = StringField('Phone', [Optional()])
+    website = StringField('Website', [Optional(), URL(message="Please enter a valid website. https://www.example.com.")])
+    image_url = StringField('Image Url', [Optional(), URL(message="Please enter a valid url.")])
+    has_address = BooleanField('Has Physical Address', default=True)
 
     def __init__(self):
         """Initialize activity drop down with database entries"""
@@ -52,3 +86,10 @@ class PlaceForm(FlaskForm):
         places = list(mongo.db.places.find({"item.name": name.data}))
         if len(places) > 0:
             raise ValidationError('Place Name already exists.')
+
+    def validate_phone(self, phone):
+        if len(phone.data) > 0:
+            if not re.match(
+                    r'^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$',
+                    phone.data):
+                raise ValidationError('Please enter a valid phone number, or clear out the entry.')
