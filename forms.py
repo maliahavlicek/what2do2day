45b2dfa3a19d6bf3fcd0d00eb71886aeb1abe4ea
maliahavlicek <malia.havlicek@gmail.com>
2020-01-18 13:23:18 -0700

    use db to populate Activities drop down, custom validator for Select place

--- forms.py
+++ forms.py
@@ -27,13 +27,28 @@ class PlaceForm(FlaskForm):
         Length(min=1, message="Description is required"),
         Length(min=2, message='Your description is too short')
     ])
+    activity_place = SelectField(u'Activity', choices=[('none','Choose Activity')], validators=[])
 
+    def __init__(self):
+        """Initialize activity drop down with database entries"""
+        super(PlaceForm, self).__init__()
 
+        from app import mongo
+        for item in list(mongo.db.activities.find({}, {'name': 1})):
+            self.activity_place.choices.append((str(item['_id']), item['name'].capitalize()))
 
+    def validate_activity_place(self, activity_place):
+        """Make sure a real selection is made."""
+        print("in validate_activity_place")
+        print("value is: " + str(activity_place.data))
+        if str(activity_place.data) == 'none':
+            raise ValidationError('Please select an activity.')
 
     def validate_name(self, name):
         """Custom validator to make sure name is unique"""
         from app import mongo
+        print("in validate_name")
+        print("value is: " + str(name.data))
         places = list(mongo.db.places.find({"item.name": name.data}))
         if len(places) > 0:
             raise ValidationError('Place Name already exists.')
