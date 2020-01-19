864f57dcf1a2beabc2bd0b16929a70357b2ebf4d
maliahavlicek <malia.havlicek@gmail.com>
2020-01-19 08:48:31 -0700

    fix postal code error detection

--- forms.py
+++ forms.py
@@ -4,18 +4,21 @@ import pymongo
 from flask_wtf import FlaskForm
 from wtforms import (StringField,
                      TextAreaField,
-                     HiddenField,
+                     IntegerField,
                      SubmitField,
                      DateField,
                      SelectField,
                      BooleanField,
                      ValidationError,
+                     HiddenField,
                      FormField)
 from wtforms.validators import (DataRequired,
                                 Email,
                                 Length,
                                 URL,
-                                Optional)
+                                Optional,
+                                NumberRange,
+                                EqualTo)
 
 from templates.validators import validate_option_not_none, validate_address
 
@@ -54,7 +57,7 @@ class AddressForm(FlaskForm):
     address_line_2 = StringField('Address line 2', [Optional()])
     city = StringField('City *', [RequiredIf(has_address=True), Length(min=1)])
     state = StringField('State/Province/Region *', [RequiredIf(has_address=True), Length(min=1)])
-    postal_code = StringField('Postal Code *', [])
+    postal_code = StringField('Postal Code *', [Optional(), Length(min=1)])
     country = SelectField('Country *', choices=[('none', 'Pick a Country.')],
                           validators=[RequiredIf(has_address=True), validate_option_not_none])
     has_address = BooleanField('Has Physical Address', default=True)
@@ -62,10 +65,25 @@ class AddressForm(FlaskForm):
     def validate_postal_code(self, postal_code):
         """Postal Code Validation"""
 
+        # limited to just alpha,numeric space and dashes for now, would be dependent upon country
+        # https://stackoverflow.com/questions/578406/what-is-the-ultimate-postal-code-and-zip-regex
         if len(postal_code.data) > 0:
             """Allow numbers with dashes only """
-            if not re.match(r'^(?=.*[09]).*[- 0-9]+$'):
+            if not re.match(r'(?i)^[a-z0-9][a-z0-9\- ]{0,10}[a-z0-9]$', postal_code.data):
                 raise ValidationError('Please enter a valid postal code.')
+
+
+class ReviewForm(FlaskForm):
+    """Review Form"""
+
+    author = StringField('Author Email *',
+                         [RequiredIf(use_place_email='y'), Length(min=1, message='Email is required.'),
+                          Email(message='Not a valid email address.')])
+    rating = IntegerField('Rating *', [NumberRange(1, 5, message='Click a star to select a rating.')])
+    comments = TextAreaField('Comments *', [Length(min=1, message='Please enter your review.'),
+                                            Length(max=500, message='Comments cannot be longer than 500 characters.')])
+    has_review = BooleanField('Add Review', default=True)
+    use_place_email = HiddenField(default='n')
 
 
 class PlaceForm(FlaskForm):
@@ -80,7 +98,8 @@ class PlaceForm(FlaskForm):
     ])
     description = TextAreaField('Description *', [
         Length(min=1, message="Description is required"),
-        Length(min=2, message='Your description is too short')
+        Length(min=2, message='Your description is too short'),
+        Length(max=500, message='Descriptions cannot be longer than 500 characters.')
     ])
     activity = SelectField(u'Activity *', choices=[('none', 'Choose Activity')], validators=[validate_option_not_none])
     phone = StringField('Phone', [Optional()])
@@ -88,20 +107,27 @@ class PlaceForm(FlaskForm):
                           [Optional(), URL(message="Please enter a valid website. https://www.example.com.")])
     image_url = StringField('Image Url', [Optional(), URL(message="Please enter a valid url.")])
     address = FormField(AddressForm, 'Address')
+    review = FormField(ReviewForm, 'Review')
 
     def __init__(self):
-        """Initialize activity drop down with database entries alphabetically"""
+        """Initialize the Place Form"""
         super(PlaceForm, self).__init__()
 
         from app import mongo
+
+        """Populate activities list from db"""
         for item in list(mongo.db.activities.find({}, {"name": 1}).sort('name', pymongo.ASCENDING)):
             self.activity.choices.append((str(item['_id']), item['name'].title()))
 
+        """Populate country list from db"""
         for item in list(mongo.db.countries.find({}, {'country': 1}).sort('country', pymongo.ASCENDING)):
             self.address.country.choices.append((
                 str(item['_id']),
                 item['country'].replace('&amp;', '&').title()
             ))
+
+        """Let review form know it should use place email"""
+        self.review.use_place_email = 'y'
 
     def validate_name(self, name):
         """Custom validator to make sure name is unique"""
