d8646717e34ec7fd3d9b5045a0032dc4ab9ccf5f
maliahavlicek <malia.havlicek@gmail.com>
2020-01-23 20:24:32 -0700

    start populating users, addresses and places..

--- app.py
+++ app.py
@@ -73,8 +73,37 @@ def get_places():
     return render_template('place/places.html', places=list_places)
 
 
-@app.route('/user/<email>', methods=['GET', 'POST'])
-def user(email):
+def place_unique(place):
+    """try to retrieve place from db via name and/or address id"""
+    the_place = mongo.db.places.find_one(place)
+    if the_place is None:
+        return None
+    else:
+        return the_place("_id")
+
+
+def add_place(place):
+    db = mongo.db.places.with_options(
+        write_concern=WriteConcern(w=1, j=False, wtimeout=5000)
+    )
+    the_place_id = db.insert_one(place)
+    return the_place_id
+
+
+def get_add_address_id(add):
+    """retrieve or create an address based on add"""
+    the_address = mongo.db.addresses.find_one(add)
+    if the_address is None:
+        db = mongo.db.addresses.with_options(
+            write_concern=WriteConcern(w=1, j=False, wtimeout=5000)
+        )
+        the_address_id = db.insert_one(add)
+    else:
+        the_address_id = the_address['_id']
+    return the_address_id
+
+
+def get_add_user_id(email):
     """retrieve or create a user based on email"""
     the_user = mongo.db.users.find_one({'email': email.lower()})
     if the_user is None:
@@ -82,7 +111,7 @@ def user(email):
             # for production application, we'd want a majority(or 2) value and True for confirmation on writing the data
             write_concern=WriteConcern(w=1, j=False, wtimeout=5000)
         )
-        the_user_id = db.insert(
+        the_user_id = db.insert_one(
             {'email': email.lower()}
         )
 
@@ -97,9 +126,56 @@ def add_place():
 
     if form.validate_on_submit():
         # all is good with the post based on PlaceForm wftForm validation
+        place = {}
+        # unique entries for a place are the name and address
+        # place name
+
+        place['name'] = form.name.data.strip().lower()
+
+        has_address = form.address.data['has_address']
+        address = {}
+        if has_address:
+            address['address_line_1'] = form.address.data['address_line_1'].strip().lower()
+            if form.address.data['address_line_2']:
+                address['address_line_2'] = form.address.data['address_line_2'].strip().lower()
+            address['city'] = form.address.data['city'].strip().lower()
+            if form.address.data['postal_code']:
+                address['postal_code'] = form.address.data['postal_code'].strip().lower()
+            address['country'] = form.address.data['country']
+            address_id = get_add_address_id(address)
+            place['address'] = address_id
+        else:
+            place['address'] = ''
+        # see if address and name is in db or not
+        is_unique = place_unique(place)
+        if is_unique is not None:
+            return render_template('error.html', reason="Place already exists.", place_id=is_unique), 1200
+
         # first get user id
-        email = form.email.data.lower()
-        user_id = user(email)
+        email = form.email.data.strip().lower()
+        place['user'] = get_add_user_id(email)
+
+        # place description
+        place['description'] = form.description.data.strip()
+
+        place['phone'] = form.phone.data.strip()
+        place['website'] = form.website.data.strip()
+        place['image_url'] = form.image_url.data.strip()
+        place['share_place'] = form.share_place.data
+
+
+
+        # now we can add the place
+        place_id = add_place(place)
+
+        # next get activity_id
+        place['activity'] = form.activity.data
+
+        # next get review
+
+        has_review = form.review.data['has_review']
+
+        has_event = form.event.data['has_event']
 
         return redirect(url_for('get_places'))
     else:
